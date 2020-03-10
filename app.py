4f2a226a5124d687cf62c7de0bc69cc91b520b2c
Atul Patel <atul@cumulusnetworks.com>
2020-03-10 12:31:23 -0700

    debug changes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ cache = redis.Redis(host='redis', port=6379)
 def check_server_load_distribution(url='http://188.188.188.1:4919/', times=1000):
     for i in range(times):
         resp=requests.get('%s' % url)
-        sleep(0.1)
+        time.sleep(0.1)
         map=recordserver(resp.json()['servername'])
 
 
